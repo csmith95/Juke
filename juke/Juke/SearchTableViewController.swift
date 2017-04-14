b059a01aa2d5a298bb7c640e06dd2d5a1e303439
kwosei <kwosei@users.noreply.github.com>
2017-04-14 08:18:34 -0700

    add new search logic

--- juke/Juke/SearchTableViewController.swift
+++ juke/Juke/SearchTableViewController.swift
@@ -2,133 +2,127 @@
 //  SearchTableViewController.swift
 //  Juke
 //
-//  Created by Conner Smith on 3/7/17.
+//  Created by Kojo Worai Osei on 4/14/17.
 //  Copyright © 2017 csmith. All rights reserved.
 //
 
 import UIKit
 import Alamofire
-import Unbox
 
-class SearchTableViewController: UITableViewController, UISearchBarDelegate {
+class SearchTableViewController: UITableViewController {
     
-    var results:[Models.SpotifySong] = []
-    let kNumResultsToStore = 20
-    let searchController = UISearchController(searchResultsController: nil)
-    
-    @IBOutlet var searchBar: UISearchBar!
-    
+    var searchURL = "https://api.spotify.com/v1/search"
+    typealias JSONStandard = [String: AnyObject]
+    var names = [String]()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        searchBar.delegate = self
-        tableView.delegate = self
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        search(query: searchBar.text!)
-        self.searchBar.endEditing(true)
-    }
-    
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
+
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        // #warning Incomplete implementation, return the number of sections
+        return 0
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return results.count
+        return names.count
+    }
+    
+    func searchSpotify(url: String) {
+        // call alamofire with endpoint
+        Alamofire.request(url).responseJSON(completionHandler: {
+            response in
+            self.parseSearchData(JSONData: response.data!)
+            
+        })
+    }
+    
+    func parseSearchData(JSONData: Data) {
+        do {
+            var readableJSON = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as! JSONStandard
+            if let tracks = readableJSON["tracks"] as? JSONStandard{
+                if let items = tracks["items"] as? [JSONStandard] {
+                    for i in 0..<items.count {
+                        let item = items[i]
+                        
+                        let name = item["name"] as! String
+                        names.append(name)
+                        
+                        self.tableView.reloadData()
+                    }
+                }
+            }
+        }
+        catch {
+            print(error)
+        }
     }
 
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = self.tableView.dequeueReusableCell(withIdentifier: "searchCell", for: indexPath) as! SearchCell
-        let song = results[(indexPath as NSIndexPath).row]
-        cell.songLabel!.text = song.songName
-        cell.artistLabel!.text = song.artistName
-        // Assign the tap action which will be executed when the user taps the UIButton
-        cell.tapAction = { (cell) in
-            // post to server
-            self.addSongToStream(song: self.results[indexPath.row], stream: CurrentUser.currStream!)
-            
-            // animate button text change from "+" to "Added!"
-            cell.addButton!.setTitle("Added!", for: .normal)
-            cell.addButton!.titleLabel?.font = UIFont(name: "System", size: 16)
-        }
-        return cell
-    }
-    
-    func addSongToStream(song: Models.SpotifySong, stream: Models.Stream) {
-        let params: Parameters = ["streamID": stream.streamID, "spotifyID": song.spotifyID, "songName": song.songName, "artistName": song.artistName, "duration": song.duration, "coverArtURL": song.coverArtURL]
-        Alamofire.request(ServerConstants.kJukeServerURL + ServerConstants.kAddSongPath, method: .post, parameters: params).validate().responseJSON { response in
-            switch response.result {
-            case .success:
-                do {
-                    let unparsedStream = response.result.value as! UnboxableDictionary
-                    let stream: Models.Stream = try unbox(dictionary: unparsedStream)
-                    CurrentUser.currStream = stream
-                    print("added song")
-                } catch {
-                    print("error unboxing stream after adding song: ", error)
-                }
-                
-            case .failure(let error):
-                print("Error adding song to current stream: ", error)
-            }
-        }
-    }
-    
-    // Code to send GET request and parse json response into results array
-    func fillItems(json: NSDictionary) {
-        let tracks = json["tracks"] as! NSDictionary
-        let items = tracks["items"] as! NSArray
-        let numItemsToCache = min(kNumResultsToStore, items.count)
-        for i in 0 ..< numItemsToCache {
-            let curr = items[i] as! UnboxableDictionary
-            
-            do {
-                let spotifySong: Models.SpotifySong = try unbox(dictionary: curr)
-                self.results.append(spotifySong)
-            } catch {
-                print("error unboxing spotify song: ", error)
-            }
-        }
-    }
-    
-    func search(query: String) {
-        if query == "" {
-            return
-        }
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SearchCell")
+        cell?.textLabel?.text = names[indexPath.row]
         
-        self.results = []
-        let params: Parameters = [
-            "query" : query,
-            "type" : "track",
-            "market" : "US",
-            "offset" : "00",
-            "limit" : "20"
-        ]
-    
-        Alamofire.request(ServerConstants.kSpotifySearchURL, method: .get, parameters: params).validate().responseJSON { response in
-            switch response.result {
-            case .success:
-                if let response = response.result.value as? NSDictionary {
-                    self.fillItems(json: response)
-                }
-                // update UI on main thread
-                DispatchQueue.main.async {
-                    self.tableView.reloadData()
-                }
-            case .failure(let error):
-                print(error)
-            }
-        }
+        return cell!
     }
-    
+ 
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
-
-
-
-
