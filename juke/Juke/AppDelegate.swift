1ce63a927e5bf3d766a7e637898baaa2d6980391
kwosei <kwosei@users.noreply.github.com>
2017-08-14 03:37:36 -0400

    add firebase

--- juke/Juke/AppDelegate.swift
+++ juke/Juke/AppDelegate.swift
@@ -10,6 +10,7 @@ import UIKit
 import CoreData
 import Alamofire
 import UserNotifications
+import Firebase
 
 
 @UIApplicationMain
@@ -19,6 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate, UNUserNotificationCenterD
     let kCallbackURL = "juke1231://callback"
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
         // Override point for customization after application launch.
         let center = UNUserNotificationCenter.current()
         center.requestAuthorization(options:[.badge, .alert, .sound]) { (granted, error) in
