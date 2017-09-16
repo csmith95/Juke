9c4d5d9e09c748862c9f9be9344d79c5b324ec9e
csmithy <csmith95@stanford.edu>
2017-09-16 14:28:22 -0700

    correctly did child view controller loading

--- juke/Juke/MyStream/MyStreamRootViewController.swift
+++ juke/Juke/MyStream/MyStreamRootViewController.swift
@@ -8,14 +8,39 @@
 
 import UIKit
 
-class MyStreamRootViewController: UIViewController {
+final class MyStreamRootViewController: UIViewController {
+    
+    var currentControllerID = ""
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        updateChildViews()
+    }
+    
+    // init child view controllers
+    private lazy var emptyStreamViewController: EmptyStreamViewController = {
+        // Load Storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
 
-    @IBOutlet var createNewStreamContainerView: UIView!
-    @IBOutlet var myStreamContainerView: UIView!
+        // Instantiate View Controller
+        var viewController = storyboard.instantiateViewController(withIdentifier: "EmptyStreamViewController") as! EmptyStreamViewController
+        
+        return viewController
+    }()
+    
+    private lazy var myStreamController: MyStreamController = {
+        // Load Storyboard
+        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+        
+        // Instantiate View Controller
+        var viewController = storyboard.instantiateViewController(withIdentifier: "MyStreamController") as! MyStreamController
+        
+        return viewController
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        NotificationCenter.default.addObserver(self, selector: #selector(MyStreamRootViewController.transitionChildViews), name: Notification.Name("userStreamChanged"), object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(MyStreamRootViewController.updateChildViews), name: Notification.Name("userStreamChanged"), object: nil)
     }
 
     override func didReceiveMemoryWarning() {
@@ -23,26 +48,43 @@ class MyStreamRootViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    func transitionChildViews() {
+    func updateChildViews() {
+        print("update")
+
+        var toRemove: UIViewController!
+        var toAdd: UIViewController!
         if Current.stream == nil || Current.stream!.song == nil {
-            UIView.animate(withDuration: 0.5, animations: {
-                self.createNewStreamContainerView.alpha = 1
-                self.myStreamContainerView.alpha = 0
-            })
+            toRemove = myStreamController
+            toAdd = emptyStreamViewController
         } else {
-            UIView.animate(withDuration: 0.5, animations: {
-                self.myStreamContainerView.alpha = 1
-                self.createNewStreamContainerView.alpha = 0
-            })
+            toRemove = emptyStreamViewController
+            toAdd = myStreamController
         }
-    }
     
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        transitionChildViews()
+        if currentControllerID == toAdd.restorationIdentifier! {
+            return  // never animate to the same controller
+        }
+        
+        toRemove.willMove(toParentViewController: nil)
+        self.addChildViewController(toAdd)
+        self.view.addSubview(toAdd.view)
+        toAdd.view.alpha = 0
+        toAdd.view.layoutIfNeeded()
+        
+        // animate transition
+        UIView.animate(withDuration: 0.5, animations: {
+            toAdd.view.alpha = 1
+            toRemove.view.alpha = 0
+        },
+        completion: { finished in
+                toRemove.view.removeFromSuperview()
+                toRemove.removeFromParentViewController()
+                toAdd.didMove(toParentViewController: self)
+        })
     }
     
     deinit {
+        print("deinit")
         NotificationCenter.default.removeObserver(self)
     }
     
